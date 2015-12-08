9188b5ab7df01b759179e3f08a619212ba928686
yogev priel <yogevp@syneron.com>
2015-12-08 16:00:34 +0200

    Fix event listner adding

--- background.js
+++ background.js
@@ -18,32 +18,31 @@ var getHost = function(url) {
   var urlObj = new URL(url);
   return urlObj.host;
 };
-
 var host =  function(tab) {
   chrome.tabs.executeScript(tab.tabId, {code: "var url = '" +  getHost(tab.url) + "'"}, function() {
     chrome.tabs.executeScript(tab.tabId, {file: "change.js"});
   });
 }
-chrome.storage.sync.get("drupal_environment_urls", function(obj) {
-  chrome.webNavigation.onCommitted.addListener(function(details) {
-    host(details);
-  }, {url: obj.drupal_environment_urls });
-});
 
-//chrome.storage.sync.set({"drupal_environment_settings": {"back": "54545", "front": "6666"}});
+var change = function(details) {
+    if (!details.frameId) {
+      host(details);
+    }
+};
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (Object.keys(request)[0] !== "background") {
+    if (!request.background) {
       return;
     };
     chrome.tabs.query({
       active: true,
+      windowType: "normal",
       lastFocusedWindow: true
     }, function(tabs) {
         var tab = tabs[0];
         var host = getHost(tab.url);
-        chrome.storage.sync.get("drupal_environment_urls", function(obj) {
+        chrome.storage.sync.get({"drupal_environment_urls":[]}, function(obj) {
           var newSite = true;
           for (var i=0; i < obj.drupal_environment_urls.length; i++) {
             if (host === obj.drupal_environment_urls[i].hostSuffix) {
@@ -53,16 +52,19 @@ chrome.runtime.onMessage.addListener(
           }
           if (newSite) {
             obj.drupal_environment_urls.push({hostSuffix: host});
-            chrome.storage.sync.set({"drupal_environment_urls": obj.drupal_environment_urls});
+            chrome.storage.sync.set({"drupal_environment_urls": obj.drupal_environment_urls}, function() {
+              chrome.webNavigation.onCommitted.removeListener(change);
+              chrome.webNavigation.onCommitted.addListener(change, {url: obj.drupal_environment_urls });
+            });
           }
         });
         var domain = {};
         domain[host] = request;
-      chrome.storage.sync.set(domain, function() {
-        chrome.tabs.executeScript(null, {code: "var url = '" +  host + "'"}, function() {
-          chrome.tabs.executeScript(null, {file: "change.js"});
+        chrome.storage.sync.set(domain, function() {
+          chrome.tabs.executeScript(tab.id, {code: "var url = '" +  host + "'"}, function() {
+            chrome.tabs.executeScript(tab.id, {file: "change.js"});
+          });
         });
       });
-    });
   });
 
